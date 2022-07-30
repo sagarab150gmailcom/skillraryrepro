73579e20556209214a868bf606033e6105abfe0f
user <user@DESKTOP-F7RQNE7>
2022-07-30 17:38:44 +0530

    FrameWork

---
+++ src/main/java/pomPages/AddtocartPage.java
@@ -0,0 +1,28 @@
+package pomPages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class AddtocartPage {
+	
+	@FindBy(id="add")
+	private WebElement addbtn;
+	
+	@FindBy(xpath="//button[@ondblclick='addtocart()']")
+	private WebElement addtocart;
+	
+	public AddtocartPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+
+	public WebElement getAddbtn() {
+		return addbtn;
+	}
+	public void addtocartbutton() {
+		addtocart.click();
+	}
+	
+
+}
